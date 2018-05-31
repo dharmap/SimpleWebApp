c67ca9c4bce9c8367431573ba32e9400efc734db
Punna <dharmasaiteja.p@gmail.com>
2018-05-30 21:56:31 -0500

    Added index page.

--- src/main/java/com/web/app/webapplication/WebApplication.java
+++ src/main/java/com/web/app/webapplication/WebApplication.java
@@ -1,4 +1,4 @@
-package com.web.app.WebApplication;
+package com.web.app.webapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
